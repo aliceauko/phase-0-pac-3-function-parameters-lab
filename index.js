761c352e574d8276c25c3df6ad432b07c6b377c0
aliceauko <alice.auko@student.moringaschool.com>
2022-05-31 14:34:28 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = Aki
+function introduction(name){
+    return  "Hi, my name is Aki.";
+}
+
+
+
+function introductionWithLanguage(name, language){
+   return "Hi, my name is Aki and I am learning to program in Ember.js.";
+}
+
+function introductionWithLanguageOptional(name, language= "JavaScript"){
+    return "Hi, my name is Aki and I am learning to program in Ember.js.";
+ }
