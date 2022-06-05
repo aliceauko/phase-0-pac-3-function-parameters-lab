99b88feedcbb9f67c63505567e066d058d2f9fd5
aliceauko <alice.auko@student.moringaschool.com>
2022-06-05 19:06:11 +0300

    commit tree 19faaf608a3582a6d30ade260f496e0f1e199a82

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let name = Aki
-function introduction(name){
-    return  "Hi, my name is Aki.";
+
+
+ function introduction(name) {
+    return `Hi, my name is ${name}.`;
 }
 
+introduction('Aki');
 
-
-function introductionWithLanguage(name, language){
-   return "Hi, my name is Aki and I am learning to program in Ember.js.";
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language= "JavaScript"){
-    return "Hi, my name is Aki and I am learning to program in Ember.js.";
- }
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
